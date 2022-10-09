6a4f71ba03f9541c0cdbb5e6386e2aff893097d7
pramudithaSJ <IT20620202@my.sliit.lk>
2022-10-09 23:04:23 +0530

    Route Path

--- frontend/hms/src/Common/AdminSide/AdminNav.js
+++ frontend/hms/src/Common/AdminSide/AdminNav.js
@@ -21,7 +21,7 @@ function AdminNav(){
                 <section><a href="/" className="Adnavbar-item"><i className="icon"><FaBookmark/></i>Room Booking</a></section>
                 <section><a href="/EmployeeView" className="Adnavbar-item"><i className="icon"><FaRegIdBadge/></i>Employees</a></section>
                 <section><a href="/" className="Adnavbar-item"><i className="icon"><FaMoneyCheckAlt/></i>payment</a></section>
-                <section><a href="/" className="Adnavbar-item"><i className="icon"><FaUserTie/></i>Customers</a></section>
+                <section><a href="/AdminPanelCustomers" className="Adnavbar-item"><i className="icon"><FaUserTie/></i>Customers</a></section>
                 </section>
         <section className="adminfooter">
             <h3> Designed By SEJ022</h3>
