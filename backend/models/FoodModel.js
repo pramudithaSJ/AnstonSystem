4b557251aaed47c8097b5f3f7e5aade13a3ce0d9
pramudithaSJ <IT20620202@my.sliit.lk>
2022-10-15 22:15:16 +0530

    Update FoodModel.js

--- backend/models/FoodModel.js
+++ backend/models/FoodModel.js
@@ -14,6 +14,10 @@ const Foodschema = new schema({
         type : String,
         required : true
     },
+    Type :{
+        type : String,
+        required : true
+    },
     Price : {
         type : String,
         required : true
